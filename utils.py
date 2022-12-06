b0852ae061c87c0b24ad4f5f6a2cf59c2ded4146
Tommy <afteryou725@gmail.com>
2022-12-06 05:53:47 +0100

    removing plotly

--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
 import altair as alt
 from cryptography.fernet import Fernet
 import io
